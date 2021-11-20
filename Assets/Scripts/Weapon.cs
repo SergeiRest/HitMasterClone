7271ddfad3c64be5d30d08234b25da4c04538c7a
SergeiRest <59287617+SergeiRest@users.noreply.github.com>
2021-11-20 22:45:19 +0300

    Добавление оружия

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+	[SerializeField] private Bullet _bullet;
+
+	public void Shot(Vector3 target)
+	{
+		var obj = Instantiate(_bullet, transform.position, Quaternion.identity);
+		obj.Flight(target);
+	}
+}
