4723aec53e796caea2eda7803828299157d51623
SergeiRest <59287617+SergeiRest@users.noreply.github.com>
2021-11-20 16:12:25 +0300

    Создание состояний

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Movement : MonoBehaviour
+{
+	[SerializeField] private Transform[] _waypoints;
+	private NavMeshAgent _agent;
+	private int _currentPoint = 0;
+	private PlayerStatesBehaviour _stateMachine = new PlayerStatesBehaviour();
+	private Camera _camera;
+    void Start()
+    {
+		_camera = Camera.main;
+		_agent = GetComponent<NavMeshAgent>();
+		this.transform.position = _waypoints[_currentPoint].position;
+		_stateMachine.Init();
+    }
+
+	private void Update()
+	{
+		_camera.transform.position = this.transform.position - new Vector3(0, -2, 5);
+	}
+
+	private void OnMouseDown()
+	{
+		switch (_stateMachine.GetPlayerState())
+		{
+			case (PlayerStates.Idle):
+				StartCoroutine(Move());
+				break;
+			case (PlayerStates.Running):
+				Debug.Log("Бежит");
+				break;
+		}
+	}
+
+	private IEnumerator Move()
+	{
+		_stateMachine.RunningState();
+		_currentPoint++;
+		_agent.SetDestination(_waypoints[_currentPoint].position);
+		yield return null;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Waypoint")
+		{
+			Debug.Log("Точка достигнута");
+			_stateMachine.IdleState();
+		}
+
+		if (_currentPoint == _waypoints.Length - 1)
+			Debug.Log("Финиш");
+	}
+}
---
+++ Assets/Scripts/Player/PlayerStatesBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerStates { Running, Shooting, Idle }
+public class PlayerStatesBehaviour
+{
+	private IPlayerState _currentState;
+	private PlayerStates _playerState;
+
+	public void Init()
+	{
+		_playerState = PlayerStates.Idle;
+		Debug.Log(_playerState);
+	}
+
+	public void ChangeState(PlayerStates state)
+	{
+		_playerState = state;
+	}
+
+	public void IdleState()
+	{
+		_currentState = new PlayerIdleState();
+		_currentState.Execute(this);
+	}
+
+	public void RunningState()
+	{
+		_currentState = new PlayerRunningState();
+		_currentState.Execute(this);
+	}
+
+	public PlayerStates GetPlayerState()
+	{
+		return _playerState;
+	}
+}
